e14effccc2cd6afe11068a88b8fdcc2e30539ed7
sebass80s <kalle.rosqvist@gmail.com>
2026-09-13 21:34:59 +0200

    Mount opt-in reconciliation diagnostics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ChildBackendQuestInbox from "@/components/ChildBackendQuestInbox";
+import ReconciliationDiagnostics from "@/components/ReconciliationDiagnostics";
 import VillagePrototype from "@/components/VillagePrototype";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <VillagePrototype />
       <ChildBackendQuestInbox />
+      <ReconciliationDiagnostics />
     </main>
   );
 }
