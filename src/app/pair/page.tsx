6d04f03bcde0cc2d15c22fc422036a4ed2df30ed
sebass80s <kalle.rosqvist@gmail.com>
2026-09-21 21:05:56 +0200

    Use Next router for web pairing return

--- src/app/pair/page.tsx
+++ src/app/pair/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import ChildPairingPanel from "@/components/ChildPairingPanel";
 
 export default function PairChildPage() {
-  return <main className="parent-page"><ChildPairingPanel onClose={() => { window.location.href = "/"; }} /></main>;
+  const router = useRouter();
+  return <main className="parent-page"><ChildPairingPanel onClose={() => router.push("/")} /></main>;
 }
