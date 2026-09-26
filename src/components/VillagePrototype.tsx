d818d2b48948fe9da1bc418a633ab758fed822f3
sebass80s <kalle.rosqvist@gmail.com>
2026-09-26 17:24:06 +0200

    Finalize release onboarding from green baseline

--- src/components/VillagePrototype.tsx
+++ src/components/VillagePrototype.tsx
@@ -677,7 +677,7 @@ export default function VillagePrototype() {
     gameRef.current?.setConstructionDialogueOpen(false);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars\n  function openSolRuntimeTest() {
+  function openSolRuntimeTest() {
     setSolRuntimeDebugEvents([]);
     setLastLiveStoryTrigger("NONE");
     solRuntimeTestActiveRef.current = true;
